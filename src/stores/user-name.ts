eafb0ddd53960cce388c534831ad0fa8d0c0f6ef
very-amused <very-amused@pm.me>
2021-12-20 14:36:51 -0500

    Add initialization to user name store
    NOTE: While working on this, I realized that I had been using casts in
    the place of proper typing for assigning request bodies.
    ```ts
    // CORRECT: assigns the object as the type, will produce errors for
    missing properties
    const document: SomeType = {
      ...properties
    }
    
    // INCORRECT: attempts to cast the object to the type, will not produce
    errors for missing properties
    const document = <SomeType>{
      ...properties
    }
    ```
    This fix should be applied to all fetch requests as a goal.

--- src/stores/user-name.ts
+++ src/stores/user-name.ts
@@ -3,7 +3,7 @@ import { getUserID } from '$lib/session'
 import { Readable, writable } from 'svelte/store'
 import { HTTPerror, NamePreferences, Visibilities } from '$lib'
 import { route } from 'core'
-import { mustGetByKey, addToStore } from '$db'
+import { mustGetByKey, addToStore, getByKey, updateWithKey } from '$db'
 
 
 function create(): Readable<Name> & NameStore {
@@ -30,8 +30,66 @@ function create(): Readable<Name> & NameStore {
         return
       }
 
-      initialized = true
+      const userID = getUserID()
+      const res = await fetch(route('/name'), {
+        method: 'GET',
+        headers: {
+          'CSRF-Token': localStorage.getItem('CSRF-Token')!
+        }
+      })
+      if (res.status === 204) {
+        initialized = true
+        return
+      }
+      if (res.status !== 200) {
+        throw new Error(await HTTPerror(res, 'failed to retrieve name from server'))
+      }
 
+      const document: NameDocument = await res.json()
+      const cached: Name|undefined = await getByKey('user-name', userID)
+      if (cached != null && cached.checksum === document.meta.checksum) {
+        set(cached)
+        return
+      }
+    
+      const visibility = document.visibility
+
+      const decryptUsername = visibility.username === Visibilities.Encrypted
+      const decryptFirstName = visibility.firstName === Visibilities.Encrypted
+      const decryptLastName = visibility.lastName === Visibilities.Encrypted
+      const hasEncryptedFields = decryptFirstName || decryptLastName || decryptUsername || document.namePreference != null
+
+      // Decrypt the cryptokey if needed
+      let cryptoKey: CryptoKey|undefined
+      if (hasEncryptedFields && document.meta.cryptoKey !== undefined) {
+        const { privateKey } = await mustGetByKey<MasterKeys>('keys', userID)
+        cryptoKey = await rsa.unwrapKey(document.meta.cryptoKey, privateKey, 'AES-GCM')
+      }
+
+      // Decrypt necessary fields
+      const firstName = decryptFirstName ? await aes.decrypt(document.firstName, cryptoKey!) : document.firstName
+      const lastName = decryptLastName ? await aes.decrypt(document.lastName, cryptoKey!) : document.lastName
+      const username = decryptUsername ? await aes.decrypt(document.username, cryptoKey!) : document.username
+      let namePreference: NamePreferences|undefined
+      if (document.namePreference != null) {
+        namePreference = parseInt(await aes.decrypt(document.namePreference, cryptoKey!))
+      }
+
+      // Update local state
+      const final: Name = {
+        id: userID,
+        firstName,
+        lastName,
+        username,
+        visibility,
+        namePreference,
+        publicNamePreference: document.publicNamePreference,
+        cryptoKey,
+        checksum: document.meta.checksum
+      }
+      set(final)
+      await updateWithKey('user-name', final)
+      initialized = true
     },
     async create(name: NameData): Promise<void> {
       // Validate the name
@@ -41,10 +99,11 @@ function create(): Readable<Name> & NameStore {
       // Get the user's ID
       const userID = getUserID()
 
-      // Generate a crypto key if there are any fields that need to be encrypted
-      const encryptUsername = name.visibility.username === Visibilities.Encrypted
-      const encryptFirstName = name.visibility.firstName === Visibilities.Encrypted
-      const encryptLastName = name.visibility.lastName === Visibilities.Encrypted
+      // Generate a key if there are any fields that need to be encrypted
+      const visibility = name.visibility
+      const encryptUsername = visibility.username === Visibilities.Encrypted
+      const encryptFirstName = visibility.firstName === Visibilities.Encrypted
+      const encryptLastName = visibility.lastName === Visibilities.Encrypted
       const hasEncryptedFields = encryptFirstName || encryptLastName || encryptUsername || name.namePreference != null
       let cryptoKey: CryptoKey|undefined
       if (hasEncryptedFields) {
@@ -72,31 +131,31 @@ function create(): Readable<Name> & NameStore {
       }
 
       // Encrypt the cryptokey
-      let encryptedKey: string|null
+      let encryptedKey: string|undefined
       if (cryptoKey != null) {
         const { publicKey } = await mustGetByKey<MasterKeys>('keys', userID)
         encryptedKey = await rsa.wrapKey(cryptoKey!, publicKey)
-      } else {
-        encryptedKey = null
       }
 
       // Submit the document to the API
+      const document: NameDocument<NameMetaRequest> = {
+        firstName,
+        lastName,
+        username,
+        namePreference: encryptedNamePreference,
+        publicNamePreference: name.publicNamePreference,
+        visibility,
+        meta: {
+          cryptoKey: encryptedKey
+        }
+      }
       const res = await fetch(route('/name'), {
         method: 'PUT',
         headers: {
           'CSRF-Token': localStorage.getItem('CSRF-Token')!,
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify(<NameDocument<NameMetaRequest>>{
-          firstName,
-          lastName,
-          username,
-          namePreference: encryptedNamePreference,
-          visibility: name.visibility,
-          meta: {
-            cryptoKey: encryptedKey
-          }
-        })
+        body: JSON.stringify(document)
       })
       if (res.status !== 200) {
         throw new Error(await HTTPerror(res, 'failed to submit name to server'))
