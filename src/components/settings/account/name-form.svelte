1b9f6d7d4ea3d6cef991fca1d6a2df2b641119cb
very-amused <very-amused@pm.me>
2021-12-21 10:17:13 -0500

    React to name visibility changes

--- src/components/settings/account/name-form.svelte
+++ src/components/settings/account/name-form.svelte
@@ -5,26 +5,35 @@
   import nameStore from '$stores/user-name'
   import { onMount } from 'svelte'
 
-  let name: Name = $nameStore
+  let name: Name
+  name = cloneName($nameStore)
+  let hasVisibilityChange: boolean
+  $: hasVisibilityChange = name.visibility.firstName !== $nameStore.visibility.firstName
+
+  // Copy a name object without any references (cryptoKey may still be a reference)
+  function cloneName(n: Name): Name {
+    return {
+      ...n,
+      visibility: { ...n.visibility }
+    }
+  }
 
   onMount(async () => {
     await nameStore.init() 
-    name = $nameStore
+    name = cloneName($nameStore)
   })
-
-
 </script>
 
 <form class="name-form" class:disabled={$navState.isEditing !== null && $navState.isEditing !== FormIDs.ChangeName}>
   <label for="firstname">First Name</label>
   <div class="input-group">
-    <input id="firstname" type="text" disabled>
-    <VisibilityForm bind:visibility={name.visibility.firstName}/>
+    <input id="firstname" type="text" bind:value={name.firstName} on:input={oninput}>
+    <VisibilityForm bind:visibility={name.visibility.firstName} on:change={oninput}/>
   </div>
 
   <label for="lastname">Last Name</label>
   <div class="input-group">
-    <input id="lastname" type="text" disabled>
+    <input id="lastname" type="text" bind:value={name.lastName}>
     <VisibilityForm bind:visibility={name.visibility.lastName}/>
   </div>
 
@@ -46,4 +55,15 @@
     <option value={DisplayNames.LastName}>Last Name</option>
     <option value={DisplayNames.FullName}>Full Name</option>
   </select>
+
+  {#if hasVisibilityChange}
+    <input type="submit" value="Submit">
+  {/if}
 </form>
+
+<style lang="scss">
+  form {
+    display: flex;
+    flex-direction: column;
+  }
+</style>
