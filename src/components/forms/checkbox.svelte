232fa8123a2fe6bbd489c3b802f3b0f6f060a93e
very-amused <very-amused@pm.me>
2022-01-05 11:34:28 -0500

    Create upgrade modal component

---
+++ src/components/forms/checkbox.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  export let value = ''
+  export let checked = false
+</script>
+
+<label class="checkable">
+  <input type="checkbox" bind:checked>
+  <span>{value}</span>
+</label>
+
+
+<style lang="scss">
+  label.checkable {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    margin: 0 !important;
+
+    span::before,span::after {
+      display: none;
+    }
+    span {
+      padding: 0;
+    }
+
+    span,input {
+      display: inline;
+    }
+    input {
+      margin: 0;
+      margin-right: 0.6rem;
+      opacity: 100%;
+      position: static;
+      width: 1rem;
+    }
+  }
+</style>
---
+++ src/components/modals/upgrade-modal.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+  import Modal from './modal.svelte'
+  import SubmitCancel from '$components/forms/submit-cancel.svelte'
+  import Checkbox from '$components/forms/checkbox.svelte'
+  import { UpgradeActions, LoginActions } from '$lib/auth-actions'
+
+  import { onMount } from 'svelte'
+
+  export let show = false
+  export let placeholder = 'Enter your password'
+
+  let actions: LoginActions
+
+  let showPassword = false
+
+  let form: HTMLFormElement
+  let passwordInput: HTMLInputElement
+
+  function cancel(): void {
+    show = false
+  }
+
+  async function submit(): Promise<void> {
+    if (!form.checkValidity()) {
+      return
+    }
+    const password = passwordInput.value
+    await UpgradeActions.passwordUpgrade(actions, password)
+    show = false
+  }
+
+  onMount(async () => {
+    actions = new LoginActions(
+      new Worker(LoginActions.Argon2_WorkerPath),
+      new Worker(LoginActions.ED25519_WorkerPath)
+    )
+    await actions.loadArgon2({
+      wasmRoot: LoginActions.Argon2_WASMRoot,
+      simd: true
+    })
+    await actions.loadED25519({
+      wasmPath: LoginActions.ED25519_WASMPath
+    })
+  })
+
+</script>
+
+<Modal bind:show lock={true}>
+  <form class="upgrade" novalidate on:submit|preventDefault={submit}>
+    <label>
+      <header>Password</header>
+      <input type="{showPassword ? 'text' : 'password'}" {placeholder} required>
+    </label>
+    <Checkbox value="Show Password" bind:checked={showPassword}/>
+    <SubmitCancel on:cancel={cancel}/>
+  </form>
+</Modal>
+
+<style lang="scss">
+  form.upgrade {
+    margin-top: 50%;
+    width: 25vw;
+    margin-right: auto;
+    margin-left: auto;
+
+    background: white;
+    color: #111;
+    padding: $padding-m;
+    text-align: left;
+  }
+
+</style>
