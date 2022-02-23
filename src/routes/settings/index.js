30fc37e0d473199aec2cdaf2b02990ead64a3e84
very-amused <very-amused@pm.me>
2022-02-23 18:31:10 -0500

    Rewrite settings index route in ts

--- src/routes/settings/index.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('@sveltejs/kit').RequestHandler} */
-export async function get() { // Redirect /settings to /settings/account with a 302 Found status code
-  return {
-    status: 302,
-    headers: {
-      'Location': '/settings/account'
-    }
-  }
-}
