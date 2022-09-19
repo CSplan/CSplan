c1419c93072d89e99579de8c13b3892be651c13a
Keith Scroggs <very-amused@pm.me>
2022-09-19 18:32:02 -0400

    Wrap logout in try/finally block

--- src/lib/page/actions.ts
+++ src/lib/page/actions.ts
@@ -6,21 +6,21 @@ import { goto, invalidateAll } from '$app/navigation'
 
 export const userActions = {
   async logout(): Promise<void> {
-    const res = await csfetch(route('/logout'), {
-      method: 'POST'
-    })
-    if (res.status !== 204) {
-      throw await HTTPerror(res, 'Failed to log out from API.')
+    try {
+      await csfetch(route('/logout'), {
+        method: 'POST'
+      })
+    } finally {
+      // Clear IDB and localstorage
+      await clearAll()
+      localStorage.clear()
+      // Redirect to home
+      await goto('/', {
+        replaceState: true
+      })
+      // Invalidate state
+      await invalidateAll()
     }
-    // Clear IDB and localstorage
-    await clearAll()
-    localStorage.clear()
-    // Redirect to home
-    await goto('/', {
-      replaceState: true
-    })
-    // Invalidate state
-    await invalidateAll()
   },
   async sendVerificationEmail(): Promise<void> {
     const res = await csfetch(route('/send-verification-email'), {
