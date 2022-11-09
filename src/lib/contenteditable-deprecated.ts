0ff44dc24414567c540f3a632745a95b3285099a
Keith Scroggs <very-amused@pm.me>
2022-11-08 20:14:08 -0500

    Remove CEtrim

--- src/lib/contenteditable-deprecated.ts
+++ src/lib/contenteditable-deprecated.ts
@@ -1,4 +1,5 @@
 /**
+ * @deprecated
  * Process keyboard actions in a contenteditable
  */
 export function CEkeypress(evt: KeyboardEvent & SafeEvent): void {
@@ -6,16 +7,6 @@ export function CEkeypress(evt: KeyboardEvent & SafeEvent): void {
     evt.currentTarget.blur()
     evt.preventDefault()
   }
-}
-
-/**
- * @deprecated
- * Trim a contenteditable element's trailer line breaks, then return the content (ready for storage)
- */
-export function CEtrim(evt: SafeEvent): string {
-  // Trim any trailing breaks
-  evt.currentTarget.innerHTML = evt.currentTarget.innerHTML.replace(/<br>$/, '')
-  return evt.currentTarget.innerHTML.replace(/<br>/g, '\n')
 }
 
 
